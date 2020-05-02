1dff5800a20e9156efd201af5773adc0e46b71d8
Melanie Bott <melaniebott@gmail.com>
2020-05-01 18:59:55 -0600

    client ID retrieved from Spotify, 2634fe2bd92b4a679078292c8734b18d

--- liri.js
+++ liri.js
@@ -1,107 +1,170 @@
 require("dotenv").config();
 // variables
 var keys = require("./keys.js");
+var axios = require("axios");
+var moment = require("moment");
 var fs = require("fs");
-var request = require("request");
+// var request = require("request");
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret,
+});
 // create the log.txt file
-var filename = './log.txt';
+var defaultMovie = "Mr. Nobody";
+// var filename = './log.txt';
+
 // module to write output to console and log.txt at the same time
-var log = require('simple-node-logger').createSimpleFileLogger(filename);
-log.setLevel('all');
+// var log = require('simple-node-logger').createSimpleFileLogger(filename);
+// log.setLevel('all');
 
-
+// Name of Venue, Location and Date
+// Use moment for date format MM/DD/YYYY
 // argv[2] choose user actions; argv[3] input paramter
-var userCommand = process.argv[2];
-var secondCommand = process.argv[3];
-for (var i = 4; i < process.argv.length; i++) {
-    secondCommand += '+' + process.argv[1];
+var action = process.argv[2];
+var value = process.argv[3];
+// switch command
+switch (action) {
+    case "concert-this":
+        getBands(value)
+        break;
+    case "spotify-this-song":
+        getSongs(value)
+        break;
+    case "movie-this":
+        if (value === "") {
+            value = defaultMovie;
+        }
+        getMovies(value)
+        break;
+    case "do-what-it-says":
+        doWhatItSays()
+        break;
+    default:
+        break;
 }
 
-// get spotify keys
-var spotify = new Spotify(keys.spotify);
-var getArtistNames = function(artist) {
-    return artist.name;
-};
+function getBands(artist) {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function(response) {
+            console.log("Name of the venue:", response.data[0].venue.name);
+            console.log("Venue location:", response.data[0].venue.city);
+            var eventDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
+            console.log("Date of the Event:", eventDate);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
 
 // function to run spotify search
-var getSpotify = function(songName) {
-    if (songName === undefined) {
-        songName = "What's my age again?";
+function getSongs(songName) {
+    // var songName = value;
+
+    // If no song is provided then your program will default to "The Sign" by Ace of Base
+    if (songName === "") {
+        songName = "The Sign";
     }
     spotify.search({
             type: "track",
-            query: userCommand
+            query: songName
         },
         function(err, data) {
             if (err) {
                 console.log("Error occurred: " + err);
                 return;
             }
-            var songs = data.tracks.items;
-            for (var i = 0; i < songs.length; i++) {
-                console.log(i);
-                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
-                console.log("song name: " + songs[i].name);
-                console.log("preview song: " + songs[i].preview_url);
-                console.log("album: " + songs[i].album.name);
-                console.log("-------------------------");
-            }
+            // Artist(s)
+            console.log("Artist(s): ", data.tracks.items[0].album.artists[0].name);
+            // The song's name
+            console.log("Song Name: ", data.tracks.items[0].song.name[0].name);
+            // Preview Link of Song from Spotify 
+            console.log("Preview Link: ", data.tracks.items[0].preview_url);
+            // The album that the song is from
+            console.log("Album Name: ", data.tracks.items[0].album.name);
+        });
+}
+
+
+// OMDB Movie
+function getMovies(movieName) {
+    axios.get("http://www.omdbapi.com/?apikey=42518777&t=" + movieName)
+        .then(function(data) {
+            var results =
+                // Title of the movie.
+                Title of the movie: $ { data.data.Title }
+                // Year the movie came out.
+            Year the movie came out: $ { data.data.Year }
+                // IMDB Rating of the movie.
+            IMDB Rating of the movie: $ { data.data.Rated }
+                // Rotten Tomatoes Rating of the movie.
+            Rotten Tomatoes Rating of the Movie: $ { data.data.Rating[1].Value }
+                // Country where the movie was produced.
+            Country where the movie was produced: $ { data.data.Country }
+                // Language of the movie.
+            Language of the movie: $ { data.data.Language }
+                // Plot of the movie.
+            Plot of the movie: $ { data.data.Plot }
+                // Actors in the movie.
+            Actors in the movie: $ { data.data.Actors };
+            console.log(results);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    if (movieName === "Mr. Nobody") {
+        console.log("------------------");
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+        console.log("It's on Netflix!");
+    };
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        data = data.split(",");
+        var action = data[0]
+        var value = data[1]
+        switch (action) {
+            case "concert-this":
+                getBands(value)
+                break;
+            case "spotify-this-song":
+                getSongs(value)
+                break;
+            case "movie-this":
+                getMovies(value)
+                break;
+            default:
+                break;
         }
-    );
-};
+    });
+}
 
-// switch command
-function mySwitch(userCommand) {
-    switch (userCommand) {
 
-        case "spotify-this-song":
-            getSpotify();
-            break;
+// queryUrl = "http://";
+// request(queryUrl, function(error, response, body) {
+//     if (!error && response.statusCode === 200) {
+//         var body = JSON.parse(body);
+//         logOutput("=============== Movie Info ===============");
+//         logOutput("Title: " + body.Title);
+//         logOutput("Release Year: " + body.Year);
+//         logOutput("IMdb Rating: " + body.imbdRating);
+//         logOutput("Rotten Tomatoes Rating: " + body.rottenTomatoes);
+//         logOutput("Country: " + body.Country);
+//         logOutput("Language: " + body.Language);
+//         logOutput("Plot: " + body.Plot);
+//         logOutput("Actors: " + body.Actors);
+//     } else {
+//         console.log("Error occurred.")
+//     }
 
-        case "movie-this":
-            getMovie();
-            break;
-        case "do-what-it-says":
-            doWhat();
-            break;
-    }
+// });
+// }
 
-    // OMDB Movie
-    function getMovie() {
-        var movieName = secondCommand;
-        var queryUrl = "http://";
-        request(queryUrl, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-                var body = JSON.parse(body);
-                logOutput("=============== Movie Info ===============");
-                logOutput("Title: " + body.Title);
-                logOutput("Release Year: " + body.Year);
-                logOutput("IMdb Rating: " + body.imbdRating);
-                logOutput("Rotten Tomatoes Rating: " + body.rottenTomatoes);
-                logOutput("Country: " + body.Country);
-                logOutput("Language: " + body.Language);
-                logOutput("Plot: " + body.Plot);
-                logOutput("Actors: " + body.Actors);
-            } else {
-                console.log("Error occurred.")
-            }
-            if (movieName === "Mr. Nobody") {
-                console.log("------------------");
-                console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-                console.log("It's on Netflix!");
-            }
-        });
-    }
 
-    function doWhat() {
-        fs.readFile("random.txt", "utf8", function(error, data) {
-            if (!error);
-            console.log(data.toString());
-            var cmds = data.toString().split(',');
-        });
-    }
 
 }
 // call switch function
 mySwitch(userCommand);
+
+https: //github.com/bdavis127/Liri-Bot/blob/master/liri.js
--- node.js
+++ node.js
@@ -1,1 +1,82 @@
-node.js
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+    id: < your spotify client id > ,
+    secret: < your spotify client secret >
+});
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+
+    console.log(data);
+});
+
+// axious Performing GET request
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+    .then(function(response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function(error) {
+        // handle error
+        console.log(error);
+    })
+    .finally(function() {
+        // always executed
+    });
+
+// Optionally the request above could also be done as
+axios.get('/user', {
+        params: {
+            ID: 12345
+        }
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+    .finally(function() {
+        // always executed
+    });
+
+// Want to use async/await? Add the `async` keyword to your outer function/method.
+async function getUser() {
+    try {
+        const response = await axios.get('/user?ID=12345');
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+// axios Performing a POST request
+axios.post('/user', {
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+//   axios Performing multiple concurrent requests
+function getUserAccount() {
+    return axios.get('/user/12345');
+}
+
+function getUserPermissions() {
+    return axios.get('/user/12345/permissions');
+}
+
+axios.all([getUserAccount(), getUserPermissions()])
+    .then(axios.spread(function(acct, perms) {
+        // Both requests are now complete
+    }));
