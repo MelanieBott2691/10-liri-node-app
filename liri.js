3ca36d086ee03c055f589a2c3bbb38db482f081d
Melanie Bott <melaniebott@gmail.com>
2020-05-02 19:47:42 -0600

    cleaned up liri file to be more readable and fixed syntax errors

--- liri.js
+++ liri.js
@@ -1,140 +1,157 @@
 require("dotenv").config();
-// variables
-var keys = require("./keys.js");
 
-// create objects
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
-var axios = require("axios");
-var moment = require("moment");
+var keys = require('./keys.js');
 var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var request = require("request");
+var imdb = require("imbd-api");
 
-// argv[2] choose user actions; argv[3] input paramter
-var type = process.argv[2];
-var term = process.argv.slice(3).join(" ");
 
-function findConcerts(search) {
-    console.log("Looking for your concerts... ");
-    //default Ace of Base
+// ============================== CONCERT SEARCH ==================================== //
+// ------------------------------- concert-this ------------------------------------ //
+/* 1. node liri.js concert-this '<artist/>band name here>'
+    This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+        * Name of venue
+        * Venue location
+        * Date of the Event (moment format as "MM/DD/YYYY") */
+
+function concertInfo(search) {
+    console.log("Looking for your concert search...");
+    /* if the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody' */
     if (!search) {
-        search = "Ace of Base"
-    };
-    var queryURL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
+        search = "Celine Dion";
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+    }
+    // Then run a request with axios to the OMDB API with the movie specified
+    var queryUrl = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
 
-    axios({
-            method: 'get',
-            url: queryURL
-        })
-        .then(function(res) {
-            // Name of Venue, Location and Date
-            // Use moment for date format MM/DD/YYYY
-            console.log("--------------New Show Listing----------------\n");
-            console.log(`Catch ${search} at: \n`);
-            for (var i = 0, i < res.data.length; i++) {
-                var venue = res.data[i].venue.name;
-                var location = res.data[i].venue.city + ", " + res.data[i].venue.region;
-                var date = moment(res.data[i].datetime).format("MM DD, YYYY");
-                console.log(`${venue} in ${location} on ${date}`);
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function(response) {
+            console.log(response);
+            var concertInfo = [
+                '\n---------------------------------------------',
+                'Venue: ' + response.data[0].venue.name,
+                'Location ' + response.data[0].venue.city + ", " + response.data[i].venue.region,
+                'Date: ' + moment(response.data[0].datetime).format("MM DD, YYYY"),
+            ].join('\n');
+            fs.appendFile('log.txt', concertInfo + '\n\n', function(err) {
+                if (err) {
+                    throw err;
+                }
+                console.log("\n-------------------------------");
+            });
+        });
+}
+// ============================== SPOTIFY SEARCH ==================================== //
+// ----------------------------- spotify-this-song ---------------------------------- //
+/* 2. node liri.js spotify-this-song '<song name here>'
+This will output the following:
+        * Artist(s)
+        * The song's name
+        * A preview of the song from Spotify
+        * The album that the song is from */
+
+function songInfo(search) {
+    console.log("Looking for your song...");
+    /* if no song is provided use default "The Sign" by Ace of Base.*/
+    if (!songName) {
+        songName = "The Sign";
+        console.log("The Sign by Ace of Base");
+    }
+    spotify.search({
+            type: 'track',
+            query: songName
+        }, function(err, data) {
+            if (err) {
+                return console.log("Error occurred: " + err);
             }
-            console.log("\n-------------------------");
+        }
+        .then(
+            function(response) {
+                console.log(response);
+                var songInfo = [
+                    '\n---------------------------------------------',
+                    'Artists: ' + data.tracks.items[0].album.artists[0].name,
+                    'Track ' + data.tracks.items[0].name,
+                    'Preview: ' + data.tracks.items[0].preview_url,
+                    'Album ' + data.tracks.items[0].album.name,
+                    console.log("-----------New Song----------\n"),
+                    // console.log(`Check out "${track}" off of the album '${album}' by '${artists}' here: ${preview}`);
+                    console.log("\n---------------------"),
+                ].join('\n');
+                fs.appendFile('log.txt', songInfo + '\n\n', function(err) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("\n-------------------------------");
+
+                });
+            })
+    );
+}
+
+
+// ============================== OMDB MOVIE SEARCH ==================================== //
+// ---------------------------------- movie-this -------------------------------------- //
+/* 3. node liri.js movie-this '<movie name here>'
+    This will output the following:
+        * Title of the movie
+        * Year the movie came out
+        * IMDB Rating of the movie
+        * Rotten Tomatoes Rating of the movie
+        * Country where the movie was produced
+        * Languaged of the movie
+        * Plot of the movie
+        * Actors in the movie */
+function movieInfo(movieName) {
+    console.log("Looking for your movie...")
+        /* if the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody' */
+    if (!movieName) {
+        movieName = "Mr. Nobody";
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+    }
+    // Then run a request with axios to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function(response) {
+            console.log(response);
+            var movieInfo = [
+                '\n---------------------------------------------',
+                'Movie Title: ' + response.data[0].Title,
+                'Year Released: ' + response.data[0].Year,
+                'IMBDRating: ' + response.data.Ratings[0].Value,
+                'Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value,
+                'Country Produced: ' + response.data[0].Country,
+                'Plot: ' + response.data[0].Plot,
+                'Actors: ' + response.data[0].Actors,
+            ].join('\n');
+            fs.appendFile('log.txt', movieInfo + '\n\n', function(err) {
+                if (err) {
+                    throw err;
+                }
+            });
         });
 }
 
-function searchSongs(search) {
-    console.log("Looking for your song...");
-    // default The Sign
-    if (!search) {
-        serach = "The Sign, Ace of Base";
-    }
-    spotify.search({
-        type: 'track',
-        query: search
-    }, function(err, data) {
-        if (err) {
-            return console.log("Error Occurred: " + err);
-        }
-        var artists = data.tracks.items[0].album.artists[0].name;
-        var track = data.tracks.items[0].name;
-        var preview = data.tracks.items[0].preview_url;
-        var album = data.tracks.items[0].album.name;
-        console.log("-----------New Song----------\n");
-        console.log(`Check out "${track}" off of the album '${album}' by '${artists}' here: ${preview}`);
-        console.log("\n---------------------");
-    });
-}
-
-// ------OMDB Movie------
-
-// Title of the movie.
-// Year the movie came out.
-// IMDB Rating of the movie.
-// Rotten Tomatoes Rating of the movie.
-// Country where the movie was produced.
-// Language of the movie.
-// Plot of the movie.
-// Actors in the movie.
-function getMovieInfo() {
-    for (var i = 3; i < input.length; i++) {
-        if (i > 2 && i < input.length) {
-            movieName = movieName + " " + input[i];
-        }
-    }
-    // if movie name is not specified then show the movie info for Mr. Nobody
-    if (!movieName) {
-        movieName = "Mr. Nobody";
-        console.log(
-            "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+// ============================== DO WHAT IT SAYS ==================================== //
+// ------------------------------ do-what-it-says ----------------------------------- //
+/* 4. node liri.js do-what-it-says
+ * using the fs node package, LIRI will take the text inside or random.txt and use it to call one of LIRI's commands.
+ ** it should run spotify-this-song for "I Want it That Way"
+ ** Edit the text in random.text to test out the feature for movie-this and concert-this */
 
 
 
-    }
-    console.log("Looking for your movie....");
-    if (!search) {
-        search = "Mr. Nobody";
-    };
-    var queryURL = "http://www.omdbapi.com/?apikey=42518777&t=" + search;
-
-    axios({
-        method: 'get',
-        url: queryURL
-    }).then(function(res) {
-        var title = res.data.Title;
-        var year = res.data.Year;
-        var imbdRating = res.data.Ratings[0].Value;
-        var rating = res.data.Ratings[1].Value;
-        var country = res.data.Country;
-        var plot = res.data.Plot;
-        var actors = res.data.Actors;
-        console.log("------------------New Movie---------------\n");
-        console.log(`${title} (${year}): ${plot} \nFilm produced in ${country} available in ${language}. Rated ${imdb} on IMDB and ${rotten} on Rotten Tomatoes. \nStarring: ${actors}.`);
-        console.log("\n------------------------------");
-    });
-}
-
-if (type === "concert-this") {
-    findConcerts(term);
-} else if (type === "spotify-this-song") {
-    findSongs(term);
-} else if (type === "movie-this") {
-    findMovies(term);
-} else if (type === "do-what-it-says") {
-    fs.readFile("random.txt", function(err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        var dataArr = data.split(",");
-        var dataAction = dataArr[0];
-        var dataTerm = dataArr[1];
-        if (dataAction === "concert-this") {
-            findConcerts(dataTerm);
-        } else if (dataACtion === "movie-this") {
-            findSongs(dataTerm);
-        } else if (dataAction === "movie-this") {
-            findMovies(dataTerm);
-        } else {
-            console.log("Error");
-        }
-    });
-} else {
-    console.log("Enter 'concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says'");
-}
+// ============================== BONUS ==================================== //
+// ----------------------------- log.txt ---------------------------------- //
+/* 
+ *in addition to logging the data to the terminal/bash window, output the data to a .txt file called log.txt
+ * make sure you append each commands you run
+ * do not overwrite your file each time you run a command */
